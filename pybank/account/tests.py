af2ed16762698a189a1e7c6115190e21340d60df
Austin Nieset <ajnieset@gmail.com>
2023-10-06 22:46:57 -0400

    added base account test

--- pybank/account/tests.py
+++ pybank/account/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from pybank.account.models import Account
+
+class AccountTestCase(TestCase):
+    def setUp(self):
+        user = User.objects.create(username="test_user", email="user@test.com")
+        Account.objects.create(balance=0.00, routing_number=12345123, account_type="checking", user=user)
+
+    def test_get_account(self):
+        account = Account.objects.get(pk=1)
+        self.assertEqual(account.balance, 0.00)
+        self.assertEqual(account.user.username, "test_user")
